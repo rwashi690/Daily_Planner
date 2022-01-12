7bcddd0bba667e79f54c2fc0b553e0ed49f83c92
rwashi690 <rwashington325@gmail.com>
2022-01-11 17:41:43 -0700

    Created a table for each daily event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
+// Get hour time blocks by id
+var hour = document.getElementById("hours")
+
 // Add today's date
 var today = moment();
-$("#currentDay").text(today.format("MMM Do, YYYY"));
+$("#currentDay").text(today.format('MMMM Do YYYY'));
+
+var objDate = new Date();
+var hours = objDate.getHours();
+    if(hours >= 9 && hours <= 17){
+        $(".hour").addClass("table-primary");  //present
+    }else if(hours < 9){
+        $(".hour").addClass("table-secondary"); //past
+    }else{
+        $(".hour").addClass("table-success"); //future
+    }
