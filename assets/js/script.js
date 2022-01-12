e1d7462e87f806340b29d153db26868e3859579e
rwashi690 <rwashington325@gmail.com>
2022-01-11 22:49:16 -0700

    Created logic that colored hours based on if they are in the current, past or future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 // Get hour time blocks by id
-var hour = document.getElementById("hours")
+var hour = document.getElementsByClassName("hours")
+// Get all the submit buttons in a query selector
+const submitBtn = document.getElementsByClassName("submit-button");
 
 // Add today's date
 var today = moment();
@@ -7,10 +9,23 @@ $("#currentDay").text(today.format('MMMM Do YYYY'));
 
 var objDate = new Date();
 var hours = objDate.getHours();
-    if(hours >= 9 && hours <= 17){
-        $(".hour").addClass("table-primary");  //present
-    }else if(hours < 9){
-        $(".hour").addClass("table-secondary"); //past
-    }else{
-        $(".hour").addClass("table-success"); //future
+
+for(let i=0; i< hour.length; i++){
+    if( hour[i].id == hours){
+        hour[i].classList.add("table-primary");  //present
+    } else if(hour[i].id < hours){
+        hour[i].classList.add("table-secondary");  //past
+    } else{
+        hour[i].classList.add("table-success"); //future
     }
+}
+
+for (let i=0; i<submitBtn.length; i++){
+    submitBtn[i].addEventListener('click', function(e){
+        console.log("from",e.target.id)
+        const textAreaId= e.target.id+"t"
+        const textAreaEl= document.getElementById(textAreaId);
+        const textAreaVal=textAreaEl.value
+        console.log(textAreaVal)
+    })
+}
